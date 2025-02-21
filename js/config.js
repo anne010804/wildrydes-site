f2d09fb87974909b9781bf79c455d743a4afcd49
anne010804 <lourdhuannejanga@gmail.com>
2025-02-21 21:22:32 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-north-1_M73jpxa7j', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7tsm4dfc59ukv1l5v4vg495i0h',
-        userPoolClientSecret: "1q7mimrbihg7k9cml33ik824r6n522ctvb5qofi42sqf7sulr829",// e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'eu-north-1_v2wXW7zwC', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3i06rfvj2kcoj96ufflv9esd0u',
+      // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
